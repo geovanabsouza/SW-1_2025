23b7901f21587807e1fa09bc428e3afcf10f9300
geovanabsouza <geovanabsouza08@gmail.com>
2025-02-13 09:07:23 -0300

    sinc2025

---
+++ exemplo01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01 - Perdido</title>
+</head>
+<body>
+    <p>Oi Turma</p>
+    <?php
+        echo "Oi Turma";
+        echo "<p> Oi Turma </p>"
+    ?>
+</body>
+</html>
