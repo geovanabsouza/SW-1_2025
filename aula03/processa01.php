c2e31854ba27d3bc6ad71e076198a0c96cf94f96
Geovana Souza <159726975+geovanabsouza@users.noreply.github.com>
2025-02-27 08:56:11 -0300

    Add files via upload

---
+++ aula03/processa01.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_REQUEST ['cxnome'];
+    $idade = $_REQUEST ['cxidade'];
+
+    echo "Nome preenchido: $nome <br>";
+    echo "Idade preenchida: $idade <br>";
+?>
---
+++ aula03/processa_exe01.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_POST ['cxnome'];
+    $msg = $_POST ['cxmsg'];
+
+    echo "Nome preenchido: $nome <br>";
+    echo "Mensagem recebida: $msg <br>";
+?>
