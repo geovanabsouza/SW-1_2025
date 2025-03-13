1e4672ee13db84d26d66252da14bba1a31968b7d
Geovana Souza <159726975+geovanabsouza@users.noreply.github.com>
2025-03-12 22:48:06 -0300

    Add files via upload

---
+++ Aula 02/exercicio03.php
@@ -0,0 +1,17 @@
+<?php
+    $x = 4;
+    $y = 3;
+
+    $soma = $x + $y;
+    $sub = $x - $y;
+    $multi = $x * $y;
+    $div = $x / $y;
+    $resto = $x % $y;
+
+    echo "A soma é $soma <br>";
+    echo "A subtração é $sub <br>";
+    echo "A multiplicação é $multi <br>";
+    echo "A divisão é $div <br>";
+    echo "O resto é $resto <br>";
+
+?>
